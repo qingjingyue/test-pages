1109988ee0d4f4682eb1bd8eb1772929622939a3
清净 <qingjing666666@126.com>
2026-02-07 20:16:59 +0800

    test-fetch

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,13 +11,15 @@ onMounted(async () => {
 
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <h1>This is an about page</h1>
+    </div>
     <div>
       用于测试 Vue 3 项目使用 GitHub Actions 在 GitHub Pages 上的自动部署
     </div>
-  </div>
-  <div>
-    <p>{{ res }}</p>
+    <div>
+      <p>{{ res }}</p>
+    </div>
   </div>
 </template>
 
