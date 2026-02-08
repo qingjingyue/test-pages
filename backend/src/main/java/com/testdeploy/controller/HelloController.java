4b9e7d2801eaeae1b1d7e64683788672450c98fc
清净 <qingjing666666@126.com>
2026-02-08 17:10:33 +0800

    deploy-b

--- backend/src/main/java/com/testdeploy/controller/HelloController.java
+++ backend/src/main/java/com/testdeploy/controller/HelloController.java
@@ -1,24 +1,30 @@
 package com.testdeploy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    static int i = 0;
+    private static final String COUNT_KEY = "visit_count";
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @GetMapping("/hello")
     public String hello() {
+        Long count = redisTemplate.opsForValue().increment(COUNT_KEY);
         return """
                 <h1> Hello World! </h1>
                 <p> 这是第 %d 次访问 </p>
-                """.formatted(++i);
+                """.formatted(count);
     }
 
     @GetMapping("/count")
-    public int count() {
-        return ++i;
+    public Long count() {
+        return redisTemplate.opsForValue().increment(COUNT_KEY);
     }
 
 }
